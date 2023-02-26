97a88590c59104021eece85e11996c58cdb076a9
Fabrice Niyongabo <ninga.fab@gmail.com>
2023-02-26 20:12:42 +0200

    init

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const FILE_UPLOAD_URL = process.env.REACT_APP_BACKEND_FILE_UPLOAD_URL;
+export const FILE_URL = process.env.REACT_APP_BACKEND_FILE_URL;
+
+export const APP_COLORS = {
+  darkBlue: "#032155",
+  lightBlue: "#112f65",
+  yellow: "#f0dc1b",
+  white: "#fff",
+  black: "#000",
+};
